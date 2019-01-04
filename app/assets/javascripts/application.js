c2454b40d35be57b0786fe20cdbce188ead40f72
Daniel Lee <ddlee929@gmail.com>
2019-01-04 16:38:39 -0600

    required jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require rails-ujs
+//= require jquery
+//= require bootstrap-sprockets
 //= require gritter
+//= require jquery_ujs
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-sprockets
+
 
