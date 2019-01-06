7981e55e6fa5fc400f0cfc149bd6bf4b2aa09b1f
Daniel Lee <ddlee929@gmail.com>
2019-01-05 21:39:41 -0600

    updated puma file again

--- config/puma.rb
+++ config/puma.rb
@@ -4,28 +4,52 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# threads threads_count, threads_count
+
+# preload_app!
+# rackup      DefaultRackup
+# # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# #
+# port        ENV.fetch("PORT") { 3000 }
+
+# # Specifies the `environment` that Puma will run in.
+# #
+# environment ENV.fetch("RAILS_ENV") { "development" }
+
+
+# threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+# threads threads_count, threads_count
+
+# on_worker_boot do
+#   # Worker specific setup for Rails 4.1+
+#   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+#   ActiveRecord::Base.establish_connection
+# end
+
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
-
-preload_app!
-rackup      DefaultRackup
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+    ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['RAILS_MAX_THREADS'] || 5
+    ActiveRecord::Base.establish_connection(config)
+
+	 
+	  if defined?(Resque)
+     Resque.redis = ENV["<redis-uri>"] || "redis://127.0.0.1:6379"
+	 ActiveRecord::Base.establish_connection
+	end
 end
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -66,3 +90,4 @@ end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+end
