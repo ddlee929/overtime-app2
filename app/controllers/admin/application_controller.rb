d598398bef5b0350aea05877faf789a97a6c6363
Daniel Lee <ddlee929@gmail.com>
2019-01-17 16:00:21 -0600

    changed application controller back

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -13,17 +13,13 @@ module Admin
     before_action :authenticate_user!
     before_action :authenticate_admin
 
-    # def authenticate_admin
-    #   unless Admin.admin_types.include?(current_user.try(:type))
-    #     flash[:alert] = "You are not authorized to access this page."
-    #     redirect_to(root_path)
-    #   end
-    # end
+    def authenticate_admin
+      unless Admin.admin_types.include?(current_user.try(:type))
+        flash[:alert] = "You are not authorized to access this page."
+        redirect_to(root_path)
+      end
+    end
 
-def authenticate_admin
-redirect_to root_path unless current_user.admin?
-end
-end
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
     # def records_per_page
